4a28f7cf3631af5b15e6f12cf818440068bba21b
Kyrylo Polishchuk <poliskyr@fit.cvut.cz>
2022-12-30 17:23:33 +0100

    Added main API constants and updated main start file

---
+++ src/API_CONSTANTS.js
@@ -0,0 +1,7 @@
+const AUTH_URL = "http://localhost:8080/api/auth/";
+const TRAINER_URL = "http://localhost:8080/api/trainer/";
+const USER_URL = "http://localhost:8080/api/user/";
+const TRAINING_URL = "http://localhost:8080/api/training/";
+const IMAGE_URL = "http://localhost:8080/api/image/";
+
+export { AUTH_URL, TRAINER_URL, TRAINING_URL, USER_URL, IMAGE_URL };
